bbc7b965f34295a18f5fc917213a5bc60702dbcf
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-04-05 19:39:29 +0530

    Test push

--- copyApiFolder.sh
+++ copyApiFolder.sh
@@ -2,6 +2,7 @@ rsync -r * API_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiF
 cp -r requirements API_DOCKER/requirements/
 cp -r static NGINX_DOCKER/static/
 cd API_DOCKER
+echo "Hello" $CODEBUILD_BUILD_ID
 docker build -t api .
 docker tag api:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:latest
 $(aws ecr get-login --region us-west-1 --no-include-email) 
