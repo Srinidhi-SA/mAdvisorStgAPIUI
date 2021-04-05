2ebef3d4bbee79a2390a84f788da280868b728d9
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-04-05 19:43:47 +0530

    Test push

--- copyApiFolder.sh
+++ copyApiFolder.sh
@@ -2,9 +2,10 @@ rsync -r * API_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiF
 cp -r requirements API_DOCKER/requirements/
 cp -r static NGINX_DOCKER/static/
 cd API_DOCKER
-echo "Hello" $CODEBUILD_BUILD_ID
+COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+IMAGE_TAG=build-$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
 docker build -t api .
-docker tag api:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:latest
+docker tag api:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:$IMAGE_TAG
 $(aws ecr get-login --region us-west-1 --no-include-email) 
-docker push 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:latest
+docker push 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:$IMAGE_TAG
 cd ..
