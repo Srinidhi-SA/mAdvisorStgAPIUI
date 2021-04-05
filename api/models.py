d178140d7ee58e5fdffed40a10245b0bac4d48ed
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-04-05 22:20:08 +0530

    Test push

--- api/models.py
+++ api/models.py
@@ -425,7 +425,7 @@ class Dataset(models.Model):
         return "/home/marlabs" + self.get_hdfs_relative_path()
 
     def get_input_file(self):
-        print("Test upload New message in API container log")
+        print("Test upload New message in API container logs")
         if self.datasource_type in ['file', 'fileUpload']:
             type = self.file_remote
             if type == 'emr_file':
