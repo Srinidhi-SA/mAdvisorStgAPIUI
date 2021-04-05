b9413c062f0489dfbe11f40f1d9879a8805b4ccb
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-04-06 00:59:30 +0530

    Test push

--- API_DOCKER/code/mAdvisor-api/README.md
+++ API_DOCKER/code/mAdvisor-api/README.md
@@ -10,3 +10,4 @@
 	NOTE :Edit /etc/hosts file with <HOST_MACHINE_IP> <Hostname> of Host machine containing any other linked containers.
 	
 	command : docker run -p 8000:8000 -it <Image_ID> bash
+# mAdvisorStgAPIUI
--- copyApiFolder.sh
+++ copyApiFolder.sh
@@ -2,10 +2,11 @@ rsync -r * API_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiF
 cp -r requirements API_DOCKER/requirements/
 cp -r static NGINX_DOCKER/static/
 cd API_DOCKER
+REPOSITORY_URI=115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api 
 COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
 IMAGE_TAG=build-$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
-docker build -t api .
-docker tag api:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:$IMAGE_TAG
+docker build -t $REPOSITORY_URI:latest .
+docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
 $(aws ecr get-login --region us-west-1 --no-include-email) 
-docker push 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api:$IMAGE_TAG
+docker push $REPOSITORY_URI:$IMAGE_TAG
 cd ..
