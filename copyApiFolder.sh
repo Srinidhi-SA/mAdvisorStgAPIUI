0f8ffeba3ed8156030fc95aa5d178e238ec3d1c9
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-04-06 12:16:09 +0530

    Test push

--- copyApiFolder.sh
+++ copyApiFolder.sh
@@ -3,10 +3,10 @@ cp -r requirements API_DOCKER/requirements/
 cp -r static NGINX_DOCKER/static/
 cd API_DOCKER
 REPOSITORY_URI=115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_api 
-COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-5)
 IMAGE_TAG=build-$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
 docker build -t $REPOSITORY_URI:latest .
-docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
+docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:12345
 $(aws ecr get-login --region us-west-1 --no-include-email) 
-docker push $REPOSITORY_URI:$IMAGE_TAG
+docker push $REPOSITORY_URI:12345
 cd ..
