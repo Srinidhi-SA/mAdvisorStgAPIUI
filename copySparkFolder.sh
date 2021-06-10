5467d9087cfd2c6512cfe26f85a71091208dd342
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-06-10 21:18:45 +0530

    Test push

--- copySparkFolder.sh
+++ copySparkFolder.sh
@@ -1,10 +1,8 @@
 #! /bin/sh
 cd Spark/
-rm -r mAdvisor-MLScripts
-git clone -b dev_ml --single-branch https://Srinidhi-SA:Sri160293@github.com/Marlabs1/mAdvisor-MLScripts
-cp Dockerfile mAdvisor-MLScripts/
-cd mAdvisor-MLScripts
-cat Dockerfile
+rm -r mAdvisorStgSpark
+git clone -b main --single-branch https://Srinidhi-SA:Sri160293@github.com/Srinidhi-SA/mAdvisorStgSpark
+cd mAdvisorStgSpark
 sh apiCode.sh
 cd SPARK_DOCKER
 docker build -t $REPOSITORY_URI_SPARK:latest .
