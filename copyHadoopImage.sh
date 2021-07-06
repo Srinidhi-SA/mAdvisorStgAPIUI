18112cb3577a7314f8f2e61a73e336e6274200ac
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-07-06 08:53:12 +0530

    Test push

--- copyHadoopImage.sh
+++
@@ -1,6 +0,0 @@
-cd hadoop_docker
-docker build -t hadoop .
-docker tag hadoop:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_hadoop:latest
-$(aws ecr get-login --region us-west-1 --no-include-email) 
-docker push 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_stg_hadoop:latest
-cd ..
